06762d91162f5f5175e7a98dfd6d9bca0580f6e8
yinmeili <2697111697@qq.com>
2019-11-16 17:04:01 +0800

    message

---
+++ build/precache-manifest.485cd2dee5d409e557cd141c09693b04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56b37388e02b1f14fc20a29570b3413",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4116eaa04bc9be231ef",
+    "url": "/static/css/2.a3a5dca9.chunk.css"
+  },
+  {
+    "revision": "c3d88fa0c3418f0f966c",
+    "url": "/static/css/main.722d7150.chunk.css"
+  },
+  {
+    "revision": "e4116eaa04bc9be231ef",
+    "url": "/static/js/2.10fd4443.chunk.js"
+  },
+  {
+    "revision": "c3d88fa0c3418f0f966c",
+    "url": "/static/js/main.39f6095e.chunk.js"
+  },
+  {
+    "revision": "14c23a88433f623ab3ae",
+    "url": "/static/js/runtime-main.e2c7d5a8.js"
+  }
+]);
